4d442a6304d99008e0b3a692a784538e916dbd34
faizafarooqchohan <161815208+faizafarooqchohan@users.noreply.github.com>
2024-03-02 22:48:35 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//Question1:install node.js, typeScript and vs code on your computer.
+//Question2:person massage,person store' name in varible,print the message to that person,your message should be single,sach as," hellow Eric, would you like to learn scme python today"?
+var person = "faiza";
+console.log("hellow ".concat(person, " would you like to learn python today?"));
---
+++ main.ts
@@ -0,0 +1,5 @@
+//Question1:install node.js, typeScript and vs code on your computer.
+
+//Question2:person massage,person store' name in varible,print the message to that person,your message should be single,sach as," hellow Eric, would you like to learn scme python today"?
+let person:string="faiza"
+console.log(`hellow ${person} would you like to learn python today?`);
